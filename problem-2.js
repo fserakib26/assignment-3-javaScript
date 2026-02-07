19307f1f6fd5bdc777792597096c29ba694bdfdf
fserakib26 <fserakib26@gmail.com>
2026-02-07 13:22:53 +0600

    Problem -2 has been solved

--- problem-2.js
+++ problem-2.js
@@ -1,6 +1,17 @@
-/*function signature/sample */
+// Problem 2 (OTP Validation)
 function validOtp(otp) {
-          // You have to write your code here
+     if (typeof otp === 'string') {
+        if (otp.length === 8 && otp.startsWith("ph-") ) {
+            return true;
+        } else {
+            return false;
+        }
+     } else {
+        return 'Invalid';
+     }
 }
+
+let output = validOtp("ph-1234");
+console.log(output);
 
 
